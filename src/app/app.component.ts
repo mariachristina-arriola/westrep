d8d2376de4fe3b5efd82d2e008d69c48e2a81df4
mariachristina-arriola <76495902+mariachristina-arriola@users.noreply.github.com>
2020-12-23 22:52:49 -0800

    it looks so dope now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit} from '@angular/core';
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,8 +10,28 @@ import { Component } from '@angular/core';
 
 
 
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild('container', {static: false}) container: ElementRef<HTMLElement>;
+  el: HTMLElement;
+
+  constructor() {
+    
+  }
+
+  ngAfterViewInit() {
+    console.log("website visited")
+  }
+
+  switchTheme() {
+    console.log('switch theme');
+    this.el.classList.toggle('theme-alternate');
+   
+  }
   title = 'Westrep Enterprises';
 
 }
+
+/**  Copyright 2019 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
 
